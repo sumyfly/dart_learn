c456efdc7acd1a53e47921381cf17c10e2cb83ef
Edward Qian <edward.qian@storehub.com>
2019-04-29 18:49:16 +0800

    update

--- Ch1/const.dart
+++ Ch1/const.dart
@@ -5,4 +5,10 @@ void main() {
   const baz = [];
   const aconstList = [1, 2, 3];
   print(aconstList);
+
+  // Immutable map
+  // const pointAndLine = const {
+  //   'point': const [const ImmutablePoint(0, 0)],
+  //   'line': const [const ImmutablePoint(1, 10), const ImmutablePoint(-2, 11)],
+  // };
 }
